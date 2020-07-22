fb083a95dcddce701087a3eb88ba01247ffcea75
yemvibol <44405282+yemvibol@users.noreply.github.com>
2020-07-22 09:57:38 +0900

    Add files via upload

---
+++ js/connect.js
@@ -0,0 +1,49 @@
+const options = Ayame.defaultOptions;
+        options.clientId = clientId ? clientId : options.clientId;
+        if (signalingKey) {
+          options.signalingKey = signalingKey;
+        }
+        let conn;
+        const disconnect = () => {
+          if (conn) {
+            conn.disconnect();
+          }
+        }
+        let dataChannel = null;
+        const label = 'dataChannel';
+        const startConn = async () => {
+          conn = Ayame.connection(signalingUrl, roomId, options, true);
+          conn.on('open', async (e) => {
+            dataChannel = await conn.createDataChannel(label);
+            if (dataChannel) {
+              dataChannel.onmessage = onMessage;
+            }
+          });
+          conn.on('datachannel', (channel) => {
+            if (!dataChannel) {
+              dataChannel = channel;
+              dataChannel.onmessage = onMessage;
+            }
+          });
+          conn.on('disconnect', (e) => {
+            console.log(e);
+            dataChannel = null;
+          });
+          await conn.connect(null);
+        };
+        <!-- const sendData = () => { -->
+		function sendSpData() {
+          const data = document.getElementById('getresult').innerHTML;
+		  console.log(data);
+          if (dataChannel && dataChannel.readyState === 'open') {
+            dataChannel.send(data);
+          }
+        };
+        document.querySelector("#roomIdInput").value = roomId;
+        document.querySelector("#clientIdInput").value = options.clientId;
+
+        function onMessage(e) {
+          const messages = document.querySelector("#messages").value;
+          newMessages = messages ? (messages + '\n' + e.data) : e.data;
+          document.querySelector("#messages").value = newMessages;
+        }
