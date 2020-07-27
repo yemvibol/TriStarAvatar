d78478ee1a1ddda0538f7f6d7ec13320e6295a1a
yemvibol <44405282+yemvibol@users.noreply.github.com>
2020-07-27 15:00:38 +0900

    Update connect.js

--- js/connect.js
+++ js/connect.js
@@ -1,49 +1,62 @@
 const options = Ayame.defaultOptions;
-        options.clientId = clientId ? clientId : options.clientId;
-        if (signalingKey) {
-          options.signalingKey = signalingKey;
-        }
-        let conn;
-        const disconnect = () => {
-          if (conn) {
-            conn.disconnect();
-          }
-        }
-        let dataChannel = null;
-        const label = 'dataChannel';
-        const startConn = async () => {
-          conn = Ayame.connection(signalingUrl, roomId, options, true);
-          conn.on('open', async (e) => {
-            dataChannel = await conn.createDataChannel(label);
-            if (dataChannel) {
-              dataChannel.onmessage = onMessage;
-            }
-          });
-          conn.on('datachannel', (channel) => {
-            if (!dataChannel) {
-              dataChannel = channel;
-              dataChannel.onmessage = onMessage;
-            }
-          });
-          conn.on('disconnect', (e) => {
-            console.log(e);
-            dataChannel = null;
-          });
-          await conn.connect(null);
-        };
-        <!-- const sendData = () => { -->
-		function sendSpData() {
-          const data = document.getElementById('getresult').innerHTML + '\n 00:';
-		  console.log(data);
-          if (dataChannel && dataChannel.readyState === 'open') {
-            dataChannel.send(data);
-          }
-        };
-        document.querySelector("#roomIdInput").value = roomId;
-        document.querySelector("#clientIdInput").value = options.clientId;
+options.clientId = clientId ? clientId : options.clientId;
+if (signalingKey) {
+    options.signalingKey = signalingKey;
+}
 
-        function onMessage(e) {
-          const messages = document.querySelector("#messages").value;
-          newMessages = messages ? (messages + '\n' + e.data) : e.data;
-          document.querySelector("#messages").value = newMessages;
+options.video.direction = 'recvonly';
+options.audio.direction = 'recvonly';
+
+let conn;
+const disconnect = () => {
+	if (conn) {
+		conn.disconnect();
+	}
+}
+let dataChannel = null;
+const label = 'dataChannel';
+const startConn = async () => {
+	
+	options.video.codec = videoCodec;
+	
+	conn = Ayame.connection(signalingUrl, roomId, options, true);
+    conn.on('open', async (e) => {
+        dataChannel = await conn.createDataChannel(label);
+        if (dataChannel) {
+            dataChannel.onmessage = onMessage;
         }
+    });
+    conn.on('datachannel', (channel) => {
+		if (!dataChannel) {
+            dataChannel = channel;
+            dataChannel.onmessage = onMessage;
+        }
+    });
+    conn.on('disconnect', (e) => {
+        console.log(e);
+        dataChannel = null;
+    });
+    await conn.connect(null);
+	
+	conn.on('open', ({authzMetadata}) => console.log(authzMetadata));
+    conn.on('addstream', (e) => {
+        document.querySelector('#remote-video').srcObject = e.stream;
+    });
+	
+};
+<!-- const sendData = () => { -->
+function sendSpData() {
+    const data = document.getElementById('getresult').innerHTML + ' \n';	  
+	console.log(data);
+    if (dataChannel && dataChannel.readyState === 'open') {
+        dataChannel.send(data);
+    }
+};
+document.querySelector("#roomIdInput").value = roomId;
+document.querySelector("#clientIdInput").value = options.clientId;
+
+function onMessage(e) {
+    const messages = document.querySelector("#messages").value;
+    newMessages = messages ? (messages + '\n' + e.data) : e.data;
+    document.querySelector("#messages").value = newMessages;
+}
