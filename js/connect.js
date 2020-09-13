1d793164391e3bc8c09dc19dc9d3b9f445497975
yemvibol <44405282+yemvibol@users.noreply.github.com>
2020-09-13 16:59:30 +0900

    Update connect.js

--- js/connect.js
+++ js/connect.js
@@ -4,44 +4,51 @@ if (signalingKey) {
     options.signalingKey = signalingKey;
 }
 
-options.video.direction = 'recvonly';
-options.audio.direction = 'recvonly';
+<!--options.video.direction = 'recvonly';-->
+<!--options.audio.direction = 'recvonly';-->
 
 let conn;
 const disconnect = () => {
-	if (conn) {
-		conn.disconnect();
-	}
+   if (conn) {
+      conn.disconnect();
+   }
 }
+
 let dataChannel = null;
 const label = 'dataChannel';
-const startConn = async () => {
-	
-	options.video.codec = videoCodec;
-	
-	conn = Ayame.connection(signalingUrl, roomId, options, true);
-    conn.on('open', async (e) => {
-        dataChannel = await conn.createDataChannel(label);
-        if (dataChannel) {
-            dataChannel.onmessage = onMessage;
-        }
-    });
-    conn.on('datachannel', (channel) => {
-		if (!dataChannel) {
-            dataChannel = channel;
-            dataChannel.onmessage = onMessage;
-        }
-    });
-    conn.on('disconnect', (e) => {
-        console.log(e);
-        dataChannel = null;
-    });
-    await conn.connect(null);
-	
-	conn.on('open', ({authzMetadata}) => console.log(authzMetadata));
-    conn.on('addstream', (e) => {
-        document.querySelector('#remote-video').srcObject = e.stream;
-    });
+const startConn = async () => {	
+   options.video.codec = videoCodec;	
+   conn = Ayame.connection(signalingUrl, roomId, options, true);	
+   
+   const mediaStream = await navigator.mediaDevices.getUserMedia({audio: true, video: false})
+   const authnMetadata = {hoge: "fuga"};
+   
+   <!-- -->	
+   conn.on('open', async (e) => {
+      dataChannel = await conn.createDataChannel(label);
+      if (dataChannel) {
+         dataChannel.onmessage = onMessage;
+      }
+   });
+   <!-- -->
+   conn.on('datachannel', (channel) => {
+      if (!dataChannel) {
+         dataChannel = channel;
+         dataChannel.onmessage = onMessage;
+      }
+   });
+   conn.on('disconnect', (e) => {
+      console.log(e);
+      dataChannel = null;
+   });
+   conn.on('open', ({authzMetadata}) => console.log(authzMetadata));
+   conn.on('addstream', (e) => {
+      document.querySelector('#remote-video').srcObject = e.stream;
+   });
+
+   <!--await conn.connect(null);-->
+   await conn.connect(mediaStream, { authnMetadata });
+   <!--document.querySelector('#local-video').srcObject = mediaStream;-->
 	
 };
 <!-- const sendData = () => { -->
